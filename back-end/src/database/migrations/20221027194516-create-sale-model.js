272ff820a375b0c188553b78550d0034cf3ec428
juniorhaussler <juniorhaussler@gmail.com>
2022-10-28 16:37:02 -0300

    feat:  Criação das migrations

---
+++ back-end/src/database/migrations/20221027194516-create-sale-model.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sales', {
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER
+      },
+      totalPrice: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+        field: 'total_price'
+      },
+      deliveryAdress: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'delivery_adress'
+      },
+      deliveryNumber: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'delivery_number'
+      },
+      saleDate: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'sale_date'
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'seller_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sales');
+  }
+};
