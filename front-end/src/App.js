66444bcdefa3352c07a2126a788c0085d0e209f2
juniorhaussler <juniorhaussler@gmail.com>
2022-10-31 17:19:30 -0300

    Feat: criação da rota /register no app.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login';
 import Products from './pages/products';
+import Register from './pages/register';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Redirect to="/login" />
         </Route>
         <Route path="/customer/products" component={ Products } />
+        <Route path="/register" component={ Register } />
       </Switch>
     </div>
   );
