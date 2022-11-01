60e7db8172600cd241beb2d6a39874b9180356ce
juniorhaussler <juniorhaussler@gmail.com>
2022-11-01 14:47:07 -0300

    Fix: correção de erros de lint.

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -1,27 +1,28 @@
 const MD5 = require('md5');
-const {User} = require('../database/models');
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
+const { User } = require('../database/models');
 
 class UserService {
-  static async getAll({name, email}) {
+  static async getAll({ name, email }) {
     const user = await User.findAll({
       where: {
         [Op.or]: [
-          {name: name},
-          {email: email}
-        ]
-      }
+          { name },
+          { email },
+        ],
+      },
     });
 
     return user;
   }
 
-  static async createUser({name, email, password}) {
-    const existUser = await this.getAll({name, email});
+  static async createUser({ name, email, password }) {
+    const existUser = await this.getAll({ name, email });
     console.log('existUser', existUser);
-    if (existUser.length>0) return {code: 409, message: 'Usuário já cadastrado'}
+    if (existUser.length > 0) return { code: 409, message: 'Usuário já cadastrado' };
     const passwordHash = MD5(password);
-    const createdUser = await User.create({name, email, password: passwordHash, role: 'customer'});
+    const createdUser = await User
+    .create({ name, email, password: passwordHash, role: 'customer' });
     return createdUser;
   }
 }
