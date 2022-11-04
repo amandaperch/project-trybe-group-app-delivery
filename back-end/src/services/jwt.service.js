7e78cd019b802ed4650a37cb1cf9bdc8ad3a5a2d
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2022-11-04 19:12:40 -0300

    feat: passa o env certo para o token

--- back-end/src/services/jwt.service.js
+++ back-end/src/services/jwt.service.js
@@ -1,16 +1,20 @@
 require('dotenv/config');
 const jwt = require('jsonwebtoken');
+const fs = require('fs')
+
+const secret = fs.readFileSync(__dirname + '/../../jwt.evaluation.key').toString()
+console.log(secret)
 
 const token = {
   create(userData) {
-  const code = jwt.sign(userData, process.env.JWT_SECRET || 'secret_key', {
+  const code = jwt.sign(userData, secret, {
      expiresIn: '7d',
      algorithm: 'HS256',
  });
   return code;
   },
   validate(userToken) {
-    const data = jwt.verify(userToken, process.env.JWT_SECRET);
+    const data = jwt.verify(userToken, secret);
     return data;
   },
 };
