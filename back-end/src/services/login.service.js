0bd46ae5b547cb1ce8fe182dfa8d67f66e872909
juniorhaussler <juniorhaussler@gmail.com>
2022-11-09 10:30:48 -0300

    Fix: refatoração do loginService.

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -1,12 +1,23 @@
 const MD5 = require('md5');
 const { User } = require('../database/models');
+const Token = require('./jwt.service');
 
 class LoginService {
    static async login(email, password) {
     const passwordEnc = MD5(password);
-    console.log(passwordEnc);
-    const searchUser = await User.findOne({ where: { email, password: passwordEnc } });
-    return searchUser;
+    const user = await User.findOne(
+      { where: { email, password: passwordEnc },
+      attributes: { exclude: ['password'] },
+      raw: false,
+    },
+);
+    const newToken = Token.create({ id: user.id,
+name: user.name, 
+      email: user.email,
+role: user.role });
+    const userData = { id: user.id, name: user.name, email: user.email, role: user.role, newToken };
+    console.log('USER:', user);
+    return userData;
   }
 }
 
