dbaa12d36e6df68d05133bc6ca79d20558f75cb8
juniorhaussler <juniorhaussler@gmail.com>
2022-11-09 10:18:49 -0300

    Fix: refatoração do código do userController.

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -2,10 +2,10 @@ const UserService = require('../services/user.service');
 
 class UserController {
   static async createUser(req, res) {
-    const data = await UserService.createUser(req.body);
-    console.log('DATA', data);
-    if ('code' in data) return res.status(data.code).json({ message: data.message });
-    return res.status(201).json({ message: 'Usuário cadastrado' });
+    const user = await UserService.createUser(req.body);
+    console.log('USER', user);
+    if ('code' in user) return res.status(user.code).json({ message: user.message });
+    return res.status(201).json(user);
   }
 }
 
