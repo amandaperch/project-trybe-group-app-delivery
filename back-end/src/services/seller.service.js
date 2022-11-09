3d6b33e184e5e21669461be488bfcb2a6801820e
juniorhaussler <juniorhaussler@gmail.com>
2022-11-09 10:34:21 -0300

    Feat: criação do sellerService.

--- back-end/src/services/seller.service.js
+++ back-end/src/services/seller.service.js
@@ -1,10 +1,10 @@
 const { User } = require('../database/models');
 
 class Sellerservice {
-  static async getAll(){
+  static async getAll() {
     const sellers = await User.findAll({
       where: { role: 'seller' },
-    },);
+    });
     return sellers;
   }
 }
