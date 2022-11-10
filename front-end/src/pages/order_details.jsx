9f8212649ef97483fc85ab70538f7d54395e5b47
Amanda Perch <perch_amanda@hotmail.com>
2022-11-10 14:43:13 -0300

    Rotas para orders e order-detais (e arquivo) criados

---
+++ front-end/src/pages/order_details.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+
+export default function OrdersDetails() {
+  return (
+    <>
+      <main>
+        <NavBar />
+      </main>
+      <p> DETALHES DO PEDIDO </p>
+    </>
+
+  );
+}
