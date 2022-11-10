6c232109e019e001a80f5ebaf7feada296d763fe
juniorhaussler <juniorhaussler@gmail.com>
2022-11-10 15:59:05 -0300

    Fix: Correção no formato da data.

--- back-end/src/database/models/sale.model.js
+++ back-end/src/database/models/sale.model.js
@@ -1,12 +1,18 @@
 // src/models/employee.model.js
 
 module.exports = (sequelize, DataTypes) => {
+  const dateObj = new Date();
+  const day = dateObj.getUTCDate();
+  const month = dateObj.getUTCMonth() + 1;
+  const year = dateObj.getUTCFullYear();
+  const fullDate = `${day}/${month}/${year}`;
+  console.log('FULLDATE', fullDate);
   const Sale = sequelize.define('Sale', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     totalPrice: DataTypes.DECIMAL(9, 2),
     deliveryAddress: DataTypes.STRING(100),
     deliveryNumber: DataTypes.STRING(50),
-    saleDate: {type: DataTypes.DATE, defaultValue: new Date().toISOString()},
+    saleDate: {type: DataTypes.DATE, defaultValue: fullDate},
     status: {type: DataTypes.STRING(50), defaultValue: 'pendente'},
     userId: { type: DataTypes.INTEGER, foreignKey: true },
     sellerId: { type: DataTypes.INTEGER, foreignKey: true },
