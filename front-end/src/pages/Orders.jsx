92e77762655773b419e26cb6c6b9199bc60101ee
Amanda Perch <perch_amanda@hotmail.com>
2022-11-10 14:15:57 -0300

    Adiciona tela de orders

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import api from '../helpers/api';
 import NavBar from '../components/navbar';
-// import CheckoutProvider from '../context/checkoutProvider';
 
 export default function Orders() {
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    async function getSales() {
+      try {
+        const res = await api.get('/sales');
+        setSales(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getSales();
+  }, []);
+
   return (
-    <main>
-      <NavBar />
-      <h1>MEUS PEDIDOS</h1>
-    </main>
+    <>
+      <main>
+        <NavBar />
+      </main>
+      <div>
+        {sales.map((item) => (
+          <div key={ item.id }>
+            <p data-testid={ `customer_orders__element-order-id-${item.id}` }>
+              {
+                item.pedido
+              }
+            </p>
+            <p data-testid={ `customer_orders__element-delivery-status-${item.id}` }>
+              {
+                item.status
+              }
+            </p>
+            <p data-tesid={ `customer_orders__element-order-date-${item.id}` }>
+              {
+                item.date
+              }
+            </p>
+            <p data-tesid={ `customer_orders__element-card-price-${item.id}` }>
+              {
+                item.price
+              }
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+
   );
 }
